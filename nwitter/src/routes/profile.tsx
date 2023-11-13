a6eef76f993c0aaafbfad1fbbf0eb2c6593d1469
zero0205 <etety39@naver.com>
2023-11-13 23:12:51 +0900

    ✨ Feat: User Profile(Image, edit name, my tweets)

--- nwitter/src/routes/profile.tsx
+++ nwitter/src/routes/profile.tsx
@@ -1,3 +1,228 @@
+import styled from "styled-components";
+import { auth, db, storage } from "../firebase";
+import { useEffect, useState } from "react";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { ITweet } from "../components/timeline";
+import Tweet from "../components/tweet";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const AvatarUpload = styled.label`
+  width: 80px;
+  overflow: hidden;
+  height: 80px;
+  border-radius: 50%;
+  background-color: #1d9bf0;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    width: 60px;
+  }
+`;
+
+const AvatarImg = styled.img`
+  width: 100%;
+`;
+
+const AvatarInput = styled.input`
+  display: none;
+`;
+
+const NameWrapper = styled.div`
+  display: flex;
+  gap: 5px;
+`;
+
+const EditNameForm = styled.form`
+  display: flex;
+`;
+
+const EditNameInput = styled.textarea`
+  border: 1px solid white;
+  height: 30px;
+  border-radius: 5px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+
+const SubmitBtn = styled.input``;
+
+const EditBtn = styled.span`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+`;
+
+const Name = styled.span`
+  font-size: 22px;
+`;
+
+const Tweets = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  gap: 10px;
+`;
+
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const user = auth.currentUser;
+  const [avatar, setAvatar] = useState(user?.photoURL);
+  const [tweets, setTweets] = useState<ITweet[]>([]);
+  const [newName, setNewname] = useState("");
+  const [editName, setEditName] = useState(false);
+
+  // 프로필 사진 바꾸기
+  const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (!user) return;
+    if (files && files.length === 1) {
+      const file = files[0];
+      const locationRef = ref(storage, `avatars/${user?.uid}`);
+      const result = await uploadBytes(locationRef, file);
+      const avatarUrl = await getDownloadURL(result.ref);
+      setAvatar(avatarUrl);
+      await updateProfile(user, { photoURL: avatarUrl });
+    }
+  };
+  // 본인의 트윗 가져오기
+  const fetchTweets = async () => {
+    const tweetQuery = query(
+      collection(db, "tweets"),
+      where("userId", "==", user?.uid),
+      orderBy("createdAt", "desc"),
+      limit(25)
+    );
+    const snapshot = await getDocs(tweetQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      const { tweet, createdAt, userId, userName, photo } = doc.data();
+      return {
+        tweet,
+        createdAt,
+        userId,
+        userName,
+        photo,
+        id: doc.id,
+      };
+    });
+    setTweets(tweets);
+  };
+
+  const onChangeName = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setNewname(e.target.value);
+  };
+
+  const onClickEditName = () => {
+    setEditName(true);
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!user) return;
+    if (!user.displayName || user.displayName !== newName) {
+      try {
+        await updateProfile(user, { displayName: newName });
+        console.log("Editted!");
+        setNewname("");
+        setEditName(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+
+  return (
+    <Wrapper>
+      <AvatarUpload htmlFor="avatar">
+        {avatar ? (
+          <AvatarImg src={avatar} />
+        ) : (
+          <svg
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z"></path>
+          </svg>
+        )}
+      </AvatarUpload>
+      <AvatarInput
+        onChange={onAvatarChange}
+        id="avatar"
+        type="file"
+        accept="image/*"
+      />
+      <NameWrapper>
+        {editName ? (
+          <EditNameForm onSubmit={onSubmit}>
+            <EditNameInput
+              onChange={onChangeName}
+              defaultValue={user?.displayName ?? ""}
+            />
+            <SubmitBtn type="submit" value="Edit" />
+          </EditNameForm>
+        ) : (
+          <>
+            <Name>{user?.displayName ?? "Anonymous"}</Name>
+            <EditBtn onClick={onClickEditName}>
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                ></path>
+              </svg>
+            </EditBtn>
+          </>
+        )}
+      </NameWrapper>
+      <Tweets>
+        {tweets.map((tweet) => (
+          <Tweet key={tweet.id} {...tweet} />
+        ))}
+      </Tweets>
+    </Wrapper>
+  );
 }
